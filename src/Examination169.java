c212ef0187660d8c1e6c9758f0958cfa1bad7759
bookdb@gomo.com <gomobookdb2019>
2020-05-11 18:37:23 +0800

    20200511

--- src/Examination169.java
+++ src/Examination169.java
@@ -4,9 +4,6 @@ import java.util.*;
  * 找出多数元素
  */
 public class Examination169 {
-    public int majorityElement(int[] nums) {
-        return 0;
-    }
 
     /**
      * 使用哈希表实现
@@ -31,4 +28,37 @@ public class Examination169 {
         });
         return allValue.get(0).getKey();
     }
+
+    /**
+     * 排序法：
+     * 因为“多数元素”是一个拥有超过n/2的数，所以对数组进行排序，第n/2一定是多数元素
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
+    /**
+     * 冒泡排序
+     * @param nums
+     * @return
+     */
+    public int[] bubbleSort(int[] nums){
+        if (nums.length == 0){
+            return nums;
+        }
+        for (int i=0;i<nums.length;i++){
+            for (int j=0;j<nums.length-1-i;j++){
+                if (nums[j+1] < nums[j]){
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+
 }
